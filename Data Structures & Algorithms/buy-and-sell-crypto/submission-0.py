201acd026356bd8ac8650d1d75e4c0ed98011580
Nandan Gowda pc <nandan.pc.gowda@gmail.com>
2026-08-26 20:42:16 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minp = 10000
+        maxp = 0
+        for i in prices:
+            minp = min(minp,i)
+            maxp = max(maxp,i-minp)
+        return maxp
+                
+
+        
